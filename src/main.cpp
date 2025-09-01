afda490be5b5404daa1f8b578779c375b0856276
gkusmierz <gk@protone.io>
2025-09-01 17:25:01 +0200

    Set application organization and name in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QSettings>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
+
+    QCoreApplication::setOrganizationName("MySoft");
+    QCoreApplication::setOrganizationDomain("mysoft.com");
+    QCoreApplication::setApplicationName("Star Runner");
+    
+    QSettings settings;
 
     QQmlApplicationEngine engine;
 
